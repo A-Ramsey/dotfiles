8be7a9bd10efe25f6f7e7c1910a5221122996d44
Aaron Ramsey <aaron.ramsey@forgehg.cloud>
2025-12-02 14:31:48 +0000

    Small updates to dotfiles

--- README.md
+++ README.md
@@ -3,7 +3,15 @@
 This repository contains my personal dotfiles for configuring various applications and tools on my system.
 These dotfiles are managed with stow and utilise multiple other tools.
 
+## Useful Features
+
+1. Keep your person git info in `~/.gitme` and this is loaded into the gitconfig
+2. Use `switch <branch name, partial branch name or new (optional)>` to switch git branches easily
+3. Use `<leader>P` in tmux to open a project finder
+4. Auto WSL detection and configuration
+
 ## Required tools
+
 - [GNU Stow](https://www.gnu.org/software/stow/)
 - [Zsh](https://www.zsh.org/)
 - [Oh My Zsh](https://ohmyz.sh/)
@@ -22,4 +30,3 @@ These dotfiles are managed with stow and utilise multiple other tools.
 ## To Do
 
 1. Write an installation script to install all the required tools and then stow the dotfiles.
-
--- zsh/scripts/fzf_projects.sh
+++ zsh/scripts/fzf_projects.sh
@@ -3,30 +3,34 @@
 window=false
 
 while getopts ':w' flag; do
-  case "${flag}" in
-    "w") window=true;;
-  esac
+	case "${flag}" in
+	"w") window=true ;;
+	esac
 done
 
 main() {
-  local dir="$(find ~/projects/ -maxdepth 1 -type d -not -path '.' -not -path '/home/$USER/projects' |fzf --preview 'ls -lah {}')"
-  if [[ -z "$dir" ]]; then
-    return 1;
-  fi
-  windowName="$(basename "$dir")"
-  windowName=$(echo "$windowName" | sed 's/\b./\u&/g' | tr '-' ' ')
-  if $window; then
-    export TMUXIFIER_PROJECT_DIR="$dir"
-    export TMUXIFIER_SESSION_NAME=${windowName^}
-    ~/dotfiles/zsh/scripts/messages.sh "info" "Opening new tmux window in $dir";
-    ~/.tmux/plugins/tmuxifier/bin/tmuxifier load-window basic-project
-    ~/dotfiles/zsh/scripts/messages.sh "success" "Opened new tmux window in $dir";
-    return 0;
-  fi
-  ~/dotfiles/zsh/scripts/messages.sh "info" "Changing directory to $dir";
-  # tmux rename-window "$windowName"
-  cd "$dir"
+	local dir="$(find ~/projects/ -maxdepth 1 -type d -not -path '.' -not -path '/home/$USER/projects' | fzf --preview 'ls -lah {}')"
+	if [[ -z "$dir" ]]; then
+		return 1
+	fi
+	windowName="$(basename "$dir")"
+	windowName=$(echo "$windowName" | sed 's/\b./\u&/g' | tr '-' ' ')
+	if $window; then
+		local windowId="$(tmux list-windows -F '#I "#W"' | awk -v name="$windowName" '$2 ~ name { print $1 }')"
+		if [[ -n "$windowId" ]]; then
+			tmux select-window -t "$windowId"
+			return 0
+		fi
+		export TMUXIFIER_PROJECT_DIR="$dir"
+		export TMUXIFIER_SESSION_NAME=${windowName^}
+		~/dotfiles/zsh/scripts/messages.sh "info" "Opening new tmux window in $dir"
+		~/.tmux/plugins/tmuxifier/bin/tmuxifier load-window basic-project
+		~/dotfiles/zsh/scripts/messages.sh "success" "Opened new tmux window in $dir"
+		return 0
+	fi
+	~/dotfiles/zsh/scripts/messages.sh "info" "Changing directory to $dir"
+	# tmux rename-window "$windowName"
+	cd "$dir"
 }
-
 
 main
